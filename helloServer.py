502fae18e053908b508839de52f47c8a1dda5a0f
Tony607 <Tony607@users.noreply.github.com>
2017-08-28 14:52:12 +0800

    ip 0.0.0.0

--- helloServer.py
+++ helloServer.py
@@ -25,7 +25,7 @@ def run():
  
   # Server settings
   # Choose port 8080, for port 80, which is normally used for a http server, you need root access
-  server_address = ('127.0.0.1', 8081)
+  server_address = ('0.0.0.0', 8081)
   httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
   print('running server...')
   httpd.serve_forever()
